19665ee017c94ea48237af8f1f7802d6aea153a7
John D. Lewis <balain@users.noreply.github.com>
2021-01-16 15:41:48 -0500

    switched to node-fetch; added list page & prev/next links

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const debug = require('debug')('app-js')
+const fetch = require('node-fetch')
 
 const fs = require('fs')
 const config = require('config')
+const NodeCache = require('node-cache')
+const cache = new NodeCache()
 
 var https = require('https')
 var http = require('http')
@@ -13,6 +16,9 @@ const app = express()
 app.use(express.static('public'))
 
 const port = config.port || 9999
+
+const HTML_FORMAT = "html"
+const JSON_FORMAT = "json"
 
 let earliestStart = false
 let latestFinish = false
@@ -99,14 +105,11 @@ function createConnectionBars (name, data, meet1StartDate, meet2StartDate, endOf
 // }
 
 function getLocalTime (inTime) {
-  // debug(`getLocalTime() called...`)
   const d = Date.parse(inTime)
-  // debug(`...d: ${d}; inTime: ${inTime}; earliestStart: ${earliestStart}; latestFinish: ${latestFinish}`)
   let response = new Date(d).toLocaleTimeString('en-US')
   if (d === earliestStart || d === latestFinish) {
     response = `<span class="timeHighlight">${response}</span>`
   }
-  // debug(`...returning ${response}`)
   return (response)
 }
 
@@ -118,7 +121,6 @@ function getLagTime (startTime, meetingStartTimeMS) {
   const d = Date.parse(startTime)
   const deltaE = (d - earliestStart) / (60 * 1000)
   const delta1 = (d - meetingStartTimeMS) / (60 * 1000)
-  // debug(`getLagTime(${startTime}, ${meetingStartTimeMS}) called... convertedStartTime: ${d} / ${new Date(d)}... meetingStartTimeMS: ${meetingStartTimeMS} / ${new Date(meetingStartTimeMS)}... deltaE: ${deltaE}, delta1: ${delta1}`)
   return { fromEarliestStart: deltaE, fromMeeting1Start: delta1 }
 }
 
@@ -130,7 +132,6 @@ async function buildToken() {
 async function buildOptions(path, method = 'GET', hostname = 'api.zoom.us') {
   debug(`buildOptions(${path}, ${method}, ${hostname}) called...`)
   const token = await buildToken()
-  debug(`...token: ${token}`)
 
   return {
       method: method,
@@ -170,17 +171,15 @@ app.get('/users', async (request, response) => {
   }
 })
 
-app.get('/mtg/:id', async (request, response) => {
-  debug(`/mtg called... ${request.params.id} `)
-
+app.get('/past_meetings_full/:id', async (request, response) => {
   const meetingIdStr = request.params.id
 
   try {
     if (config.meetingIds && config.meetingIds.includes(meetingIdStr)) {
       const meetingId = Number(meetingIdStr)
       if (!isNaN(meetingId)) {
-        var options = await buildOptions(`/v2/report/meetings/${meetingId}/participants?page_size=100`)
-
+        var options = await buildOptions(`/v2/meetings/${meetingId}?show_previous_occurrences=true`)
+        debug(`options: `, options)
         var req = https.request(options, function (res) {
           var chunks = []
 
@@ -191,81 +190,239 @@ app.get('/mtg/:id', async (request, response) => {
           res.on('end', function () {
             var body = Buffer.concat(chunks);
             const result = JSON.parse(body.toString())
-            const namesArr = []
-            if (result.participants && result.participants.length > 0) {
-              earliestStart = Date.parse(result.participants[0].join_time)
-              latestFinish = Date.parse(result.participants[0].leave_time)
-
-              for (var p = 0; p < result.participants.length; p++) {
-                const partname = result.participants[p].name
-                if (!Object.keys(namesArr).includes(partname)) {
-                  // debug(`...adding ${partname} to namesArr`)
-                  namesArr[partname] = { totaltime: 0, data: [] }
-                }
-                namesArr[partname].data.push(result.participants[p])
-                namesArr[partname].totaltime += result.participants[p].duration
-
-                const myStart = Date.parse(result.participants[p].join_time)
-                const myEnd = Date.parse(result.participants[p].leave_time)
-
-                if (myStart < earliestStart) {
-                  earliestStart = myStart
-                }
-                if (myEnd > latestFinish) {
-                  latestFinish = myEnd
-                }
-              }
-
-              // Now calculate the meeting start times
-              const e = new Date(earliestStart)
-              const meet1StartDate = new Date(e.getFullYear(), e.getMonth(), e.getDate(), meet1Start.hour, meet1Start.minute, 0)
-              const meet2StartDate = new Date(e.getFullYear(), e.getMonth(), e.getDate(), meet2Start.hour, meet2Start.minute, 0)
-              // debug(`meet1StartDate = ${meet1StartDate}`)
-              // debug(`meet2StartDate = ${meet2StartDate}`)
-
-              const names = [...new Set(Object.keys(namesArr))].sort()
-              response.write(`<!doctype html><html lang="en"><head><title>Attendees</title><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-              <style>
-              .timeblank { background-color: green; empty-cells: show;padding: 10px;align-content: center;vertical-align: middle;margin: 2px; }
-              .totaltimeservice { background-color: blue; empty-cells: show;padding: 10px;align-content: center;vertical-align: middle;margin: 2px; }
-              .totaltimeboth { background-color: lightblue; empty-cells: show;padding: 10px;align-content: center;vertical-align: middle;margin: 2px; }
-              .timeHighlight { color: blue; font-weight: bold; }
-              </style>
-              </head><body>`)
-              response.write(`<H1>Participants (Meeting: ${meetingId})</H1><EM>Count: ${names.length}</EM>`)
-
-              response.write(`<p>Earliest Join: ${getLocal(earliestStart)}<BR>Latest Leave: ${getLocal(latestFinish)}</p>`)
-              response.write('<TABLE class="table table-striped table-sm" style="width: auto !important;"><thead class="table-dark"><TR><TH colspan=2>Name</TH></TR></thead>')
-              response.write('<tbody class="table-light">')
-
-              const t = namesArr[names[0]].data[0].leave_time
-              const localOffset = new Date().getTimezoneOffset() * 60000
-              const endOfServices = new Date(Date.UTC(t.substr(0, 4), t.substr(5, 2), t.substr(8, 2), 11, 35, 0)).getTime() + localOffset
-
-              names.forEach((name) => {
-                response.write(`<TR><TD>${convertNameDataToRow(name, namesArr[name])}</TD>`)
-                response.write('<TD><div>')
-                response.write(createConnectionBars(name, namesArr[name], meet1StartDate, meet2StartDate, endOfServices))
-                response.write('</div></TD></TR>')
-              })
-              response.write('</TD></TR>')
-              response.write('</tbody></TABLE>')
-              response.write('<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script><script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script></body></html>')
-              response.end()
-            } else {
-              response.send('No participants found')
-            }
+            debug(`Occurrences? `, response.occurrences ? 'YES has occurrences' : 'NO occurrences')
+            response.send(result)
+            // response.send(result.meetings.sort((a, b) => { return (b.start_time < a.start_time) ? -1 : (b.start_time > a.start_time) ? 1 : 0 }))
           })
         })
-        req.end()
-      } else {
-        response.send('Invalid meeting ID (Not a valid number)')
       }
+    }
+    req.end()
+  } catch (err) {
+    response.send(err)
+  }
+})
+
+app.get('/past_meetings/:id', async (request, response) => {
+  const meetingIdStr = request.params.id
+  const format = request.query.format ? request.query.format : HTML_FORMAT
+
+  try {
+    if (config.meetingIds && config.meetingIds.includes(meetingIdStr)) {
+      const meetingId = Number(meetingIdStr)
+      if (!isNaN(meetingId)) {
+        const cacheId = `past_meetings-${meetingId}`
+        if (!cache.has(cacheId)) {
+          debug(`...creating cache ${cacheId}`)
+
+          var options = await buildOptions(`/v2/past_meetings/${meetingId}/instances`)
+          var req = https.request(options, function (res) {
+            var chunks = []
+            var cacheContent = []
+
+            res.on('data', function (chunk) {
+              chunks.push(chunk)
+            })
+
+            res.on('end', function () {
+              var body = Buffer.concat(chunks);
+              const result = JSON.parse(body.toString())
+              
+              let meetings = result.meetings.sort((a, b) => { return (b.start_time < a.start_time) ? -1 : (b.start_time > a.start_time) ? 1 : 0 })
+
+              if (format == HTML_FORMAT) {
+                let id = meetings.length
+                let seq = 0
+                cacheContent.push('<table><thead><tr><th>#</th><th>Date</th></tr></thead><tbody>')
+                meetings.forEach((mtg) => {
+                  // debug(mtg)
+
+                  cacheContent.push(`<tr><td>${id}</td><td><a href='/mtg?id=${meetingId}&instance=${encodeURIComponent(mtg.uuid)}'>${mtg.start_time.substr(0, 10)}</a></td></tr>`)
+                  id--
+                  seq++
+                })
+                cacheContent.push('</tbody></table>')
+                cache.set(cacheId, cacheContent.join(''))
+                response.write(cacheContent.join(''))
+                response.end()
+              } else {
+                response.send(meetings)
+              }
+            })
+          })
+          req.end()
+        } else {
+          debug(`...returning content from cache (ID: ${cacheId})`)
+          response.write(cache.get(cacheId))
+        }
+      }
+    }
+  } catch (err) {
+    response.send(err)
+  }
+})
+
+async function getMeetingHistory(meetingId) {
+  const cacheId = `meetingHistory-${meetingId}`
+  return new Promise(async (resolve, reject) => {
+    if (!cache.has(cacheId)) {
+      const path = `/v2/past_meetings/${meetingId}/instances`
+      var options = await buildOptions(path)
+      const response = await fetch(`https://api.zoom.us${path}`, options)
+      const json = await response.json()
+      cache.set(cacheId, json)
+      debug(`...cache updated`)
     } else {
-      response.send('Unrecognized meeting ID')
+      debug(`...returning from cache`)
+    }
+    resolve(cache.get(cacheId))
+  })
+}
+
+async function getPrevNextMeeting(meetingId, meetingInstance) {
+  let history = await getMeetingHistory(meetingId)
+
+  debug(`getPrevNextMeeting(${meetingId}, ${meetingInstance}) called...`)
+
+  meetings = history.meetings.sort((a, b) => { return (b.start_time > a.start_time) ? -1 : (b.start_time < a.start_time) ? 1 : 0 })
+
+  let response = { prev: false, next: false }
+
+  let ndx = meetings.length - 1
+
+  if (meetingInstance) {
+    ndx = meetings.findIndex(x => x.uuid == meetingInstance)
+  }
+
+  if (ndx > 0) { response.prev = meetings[ndx - 1] }
+  if (ndx < meetings.length - 1) { response.next = meetings[ndx + 1] }
+
+  debug(`...returning: `, response)
+  return(response)
+}
+
+app.get('/history/:id', async (request, response) => {
+  res.write(`history: `)
+  res.send(await getMeetingHistory(request.params.id))
+})
+
+app.get('/mtg', async (request, response) => {
+  debug(`/mtg called...`)
+
+  const meetingId = request.query.id ? request.query.id : false
+  const meetingInstance = request.query.instance ? request.query.instance : false
+  const meeting = meetingInstance || meetingId
+
+  debug(`meetingId: ${meetingId}; meetingInstance: ${meetingInstance}; meeting: ${meeting}`)
+
+  // let meetingHistory = await getMeetingHistory(meetingId)
+  let prevNextMeetings = false
+
+  // if (meetingInstance) {
+    prevNextMeetings = await getPrevNextMeeting(meetingId, meetingInstance)
+  // }
+
+  try {
+    var path = `/v2/report/meetings/${meeting}/participants?page_size=100`
+    const options = await buildOptions(path)
+    const fetchResponse = await fetch(`https://api.zoom.us${path}`, options)
+    const result = await fetchResponse.json()
+
+    const namesArr = []
+    if (result.participants && result.participants.length > 0) {
+      earliestStart = Date.parse(result.participants[0].join_time)
+      latestFinish = Date.parse(result.participants[0].leave_time)
+
+      for (var p = 0; p < result.participants.length; p++) {
+        const partname = result.participants[p].name
+        if (!Object.keys(namesArr).includes(partname)) {
+          // debug(`...adding ${partname} to namesArr`)
+          namesArr[partname] = { totaltime: 0, data: [] }
+        }
+        namesArr[partname].data.push(result.participants[p])
+        namesArr[partname].totaltime += result.participants[p].duration
+
+        const myStart = Date.parse(result.participants[p].join_time)
+        const myEnd = Date.parse(result.participants[p].leave_time)
+
+        if (myStart < earliestStart) {
+          earliestStart = myStart
+        }
+        if (myEnd > latestFinish) {
+          latestFinish = myEnd
+        }
+      }
+
+      // Now calculate the meeting start times
+      const e = new Date(earliestStart)
+      const meet1StartDate = new Date(e.getFullYear(), e.getMonth(), e.getDate(), meet1Start.hour, meet1Start.minute, 0)
+      const meet2StartDate = new Date(e.getFullYear(), e.getMonth(), e.getDate(), meet2Start.hour, meet2Start.minute, 0)
+
+      const names = [...new Set(Object.keys(namesArr))].sort()
+      response.write(`<!doctype html><html lang="en"><head><title>Attendees</title><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+      <style>
+      .timeblank { background-color: green; empty-cells: show;padding: 10px;align-content: center;vertical-align: middle;margin: 2px; }
+      .totaltimeservice { background-color: blue; empty-cells: show;padding: 10px;align-content: center;vertical-align: middle;margin: 2px; }
+      .totaltimeboth { background-color: lightblue; empty-cells: show;padding: 10px;align-content: center;vertical-align: middle;margin: 2px; }
+      .timeHighlight { color: blue; font-weight: bold; }
+      </style>
+      </head><body>`)
+      response.write(`<H1>Participants (Meeting: ${meetingId})</H1>`)
+      response.write(`<div>`)
+      if (prevNextMeetings.prev) {
+        response.write(`<a href='/mtg?id=${meetingId}&instance=${encodeURIComponent(prevNextMeetings.prev.uuid)}'>Prev meeting (${prevNextMeetings.prev.start_time.substr(0,10)})</a>`)
+      }
+      response.write(`<span style='padding: 5px;'>Date: ${namesArr[names[0]].data[0].join_time.substr(0,10)}</span>`)
+      if (prevNextMeetings.next) {
+        response.write(`<a href='/mtg?id=${meetingId}&instance=${encodeURIComponent(prevNextMeetings.next.uuid)}'>Next meeting (${prevNextMeetings.next.start_time.substr(0,10)})</a>`)
+      }
+      response.write(`</div>`)
+      response.write(`<EM> Count: ${ names.length }</EM>`)
+
+      response.write(`<p>Earliest Join: ${getLocal(earliestStart)}<BR>Latest Leave: ${getLocal(latestFinish)}</p>`)
+      response.write('<TABLE class="table table-striped table-sm" style="width: auto !important;"><thead class="table-dark"><TR><TH colspan=2>Name</TH></TR></thead>')
+      response.write('<tbody class="table-light">')
+
+      const t = namesArr[names[0]].data[0].leave_time
+      const localOffset = new Date().getTimezoneOffset() * 60000
+      const endOfServices = new Date(Date.UTC(t.substr(0, 4), t.substr(5, 2), t.substr(8, 2), 11, 35, 0)).getTime() + localOffset
+
+      names.forEach((name) => {
+        response.write(`<TR><TD>${convertNameDataToRow(name, namesArr[name])}</TD>`)
+        response.write('<TD><div>')
+        response.write(createConnectionBars(name, namesArr[name], meet1StartDate, meet2StartDate, endOfServices))
+        response.write('</div></TD></TR>')
+      })
+      response.write('</TD></TR>')
+      response.write('</tbody></TABLE>')
+      response.write('<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script><script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script></body></html>')
+      response.end()
+    } else {
+      // No participants
+      response.write(`<!doctype html><html lang="en"><head><title>Attendees</title><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+      <style>
+      .timeblank { background-color: green; empty-cells: show;padding: 10px;align-content: center;vertical-align: middle;margin: 2px; }
+      .totaltimeservice { background-color: blue; empty-cells: show;padding: 10px;align-content: center;vertical-align: middle;margin: 2px; }
+      .totaltimeboth { background-color: lightblue; empty-cells: show;padding: 10px;align-content: center;vertical-align: middle;margin: 2px; }
+      .timeHighlight { color: blue; font-weight: bold; }
+      </style>
+      </head><body>`)
+      // response.write('No participants found')
+      response.write(`<div>`)
+      if (prevNextMeetings.prev) {
+        response.write(`<a href='/mtg?id=${meetingId}&instance=${prevNextMeetings.prev.uuid}'>Prev meeting (${prevNextMeetings.prev.start_time.substr(0,10)})</a>`)
+      }
+      // response.write(`<span style='padding: 5px;'>Date: ${namesArr[names[0]].data[0].join_time.substr(0,10)}</span>`)
+      if (prevNextMeetings.next) {
+        response.write(`<a href='/mtg?id=${meetingId}&instance=${prevNextMeetings.next.uuid}'>Next meeting (${prevNextMeetings.next.start_time.substr(0,10)})</a>`)
+      }
+      response.write(`</div>`)
+      response.write('<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script><script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script></body></html>')
+      response.end()
     }
   } catch (error) {
-    response.send('Error encountered')
+    response.write('Error encountered')
+    response.end()
     debug(`Error: ${error}`)
   }
 })
